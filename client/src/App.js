d817a2e40782351ce1f56c97d12436ef9bb64f4e
dicky <dickyjuwono@gmail.com>
2020-03-22 23:52:23 +0700

    WIP

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,106 @@
-import React from 'react';
-import Select from './components/Select';
-
+import React from "react";
+import Select from "./components/Select";
+import logo from "./logo@2x.png";
+import balloon from "./balloon@2x.png";
+import hero from "./hero_01_girl.png";
+import boxes from "./boxes.png";
 
 function App() {
   return (
-    <div className="App" >
-    <div className="w-full max-w-md bg-gray-800" >
-      <form action="" className=" bg-white shadow-md rounded px-8 py-8 pt-8">
-        <div className="px-4 pb-4">
-          <label htmlFor="email" className="text-sm block font-bold  pb-2">EMAIL ADDRESS</label>
-          <input type="email" name="email" id="" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 " placeholder="Johnbull@example.com"/>
+    <div className="App">
+      <div>
+        <div className="flex justify-between py-4 px-8 bg-white border-b-2 shadow-lg">
+          <div>
+            <img className="w-40" src={logo} />
+          </div>
+          <div className="flex items-center">
+            <div className="mx-4 text-md font-medium text-gray-500">menu 1</div>
+            <div className="mx-4 text-md font-medium text-gray-500">menu 2</div>
+            <div className="mx-4 text-md font-medium text-gray-500">menu 3</div>
+            <div className="mx-4 text-md font-medium text-gray-500">
+              Subscribe
+            </div>
+            <button className="py-2 px-12 bg-red-700 rounded-full text-white ml-4 ">
+              Login
+            </button>
+          </div>
         </div>
-        <div  className="px-4 pb-4">
-          <label htmlFor="password" className="text-sm block font-bold pb-2">PASSWORD</label>
-          <input type="password" name="email" id="" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300" placeholder="Enter your password"/>
+
+        {/* Hero section */}
+        {/* hero left */}
+        <div className="flex items-stretch w-4/5  mt-12 mx-auto items-end">
+          <div className="flex-1 pb-12 mr-4">
+            <h1 className="text-5xl font-bold text-red-700 leading-tight mb-6">
+              Fighting isolation with Books, DVDs, and Games
+            </h1>
+            <h3 className="text-xl text-gray-500 font-md mb-12">
+              Every week a friendly neighbour will collect items from local
+              charity shops and drop them at your door.
+            </h3>
+            <div className="w-64 py-4 px-8 bg-red-700 rounded-full text-white text-center">
+              Subscribe Now
+            </div>
+          </div>
+          {/* hero right */}
+          <div className="relative flex-1">
+            <img className="absolute z-10 left-0 bottom-0 w-32 " src={boxes} />
+            <img className="absolute z-10 right-0 bottom-0 w-64 " src={hero} />
+            <img className="absolute left-0.top-0 w-56" src={balloon} />
+          </div>
         </div>
-        <div>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Sign In</button>
+
+        {/* Input section */}
+      
+
+        <div className="bg-gray-200 flex w-full">
+          <form
+            action=""
+            className="flex items-end mx-auto px-8 py-8 pt-8"
+          >
+            <div className="px-4">
+              <label htmlFor="email" className="text-sm block font-bold  pb-2">
+                EMAIL ADDRESS
+              </label>
+              <input
+                type="email"
+                name="email"
+                id=""
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 "
+                placeholder="Johnbull@example.com"
+              />
+            </div>
+
+            <div className="px-4">
+              <label
+                htmlFor="password"
+                className="text-sm block font-bold pb-2"
+              >
+                PASSWORD
+              </label>
+              <input
+                type="password"
+                name="email"
+                id=""
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300"
+                placeholder="Enter your password"
+              />
+            </div>
+            <div>
+              <button
+                className="border-2 border-gray-400 text-gray-400 px-12  rounded-full hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
+
+
+      
+
+      </div>
     </div>
-  </div>
   );
 }
 
