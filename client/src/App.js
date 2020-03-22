fd2ccd184c5b871b97573d6e29c6789e1cc71872
Blake <bpro0821@gmail.com>
2020-03-22 17:02:55 -0500

    stash

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
           <input type="password" name="email" id="" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300" placeholder="Enter your password"/>
         </div>
         <div>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Sign In</button>
+          <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Sign In</button>
         </div>
       </form>
     </div>
