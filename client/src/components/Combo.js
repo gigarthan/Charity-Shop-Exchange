048f81981c7d07949a97590524acc5d59344d614
Blake <bpro0821@gmail.com>
2020-03-23 11:00:43 -0500

    cleanup and minor styling change

--- client/src/components/Combo.js
+++ client/src/components/Combo.js
@@ -59,27 +59,27 @@ export default function Combo() {
   return (
     <>
       <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
+        <label style={{color: 'white'}} className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
           Country
       </label>
         <div className="relative">
-          <vaadin-combo-box name="countryId" ref={countryCombo} placeholder="select" className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state" />
+          <vaadin-combo-box name="countryId" style={{backgroundColor: '#d2d9e1'}} ref={countryCombo} placeholder="select" className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state" />
         </div>
       </div>
       <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
+        <label style={{color: 'white'}} className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
           County
       </label>
         <div className="relative">
-          <vaadin-combo-box name="countyId" ref={countyCombo} placeholder="select" className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state" />
+          <vaadin-combo-box name="countyId" style={{backgroundColor: '#d2d9e1'}} ref={countyCombo} placeholder="select" className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state" />
         </div>
       </div>
       <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
+        <label style={{color: 'white'}} className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
           Town
       </label>
         <div className="relative">
-          <vaadin-combo-box name="townId" ref={townCombo} placeholder="select" className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state" />
+          <vaadin-combo-box name="townId" style={{backgroundColor: '#d2d9e1'}} ref={townCombo} placeholder="select" className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state" />
         </div>
       </div>
     </>
