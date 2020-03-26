3f5ad45b42968d56449a04fd4adb69b096f5b90e
Matt Doyle <matt@elated.com>
2020-03-26 12:06:09 +1100

    Added 'label' attribute to Combo

--- client/src/components/Combo.js
+++ client/src/components/Combo.js
@@ -14,6 +14,7 @@ export default function Combo(props) {
       placeholder,
       style,
       theme='',
+      label='',
       className
   } = props;
 
@@ -71,6 +72,7 @@ export default function Combo(props) {
       style={style}
       className={className}
       theme={theme}
+      label={label}
     />
   );
 
