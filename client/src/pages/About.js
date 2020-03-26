4dd9a7f793ac6759b57567e62238a53e1da0aa2b
dicky <dickyjuwono@gmail.com>
2020-03-27 01:45:46 +0700

    wip about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -13,7 +13,6 @@ import marina from "../assets/img/marina@2x.png";
 export default function About() {
   return (
     <Layout>
-      <container className="mx-auto">
       <h1 className="text-2xl sm:text-4xl px-3 font-bold text-center mt-12 mb-8 sm:mb-20 text-red-700 leading-snug">
         Why we built Charity Shop Exchange
       </h1>
@@ -46,169 +45,220 @@ export default function About() {
         behalf of a syndication of suppliers. And we're open to working together
         on digital projects that see the value in this approach.
       </p>
+
+    <div className="bg-gray-200 w-screen mx-auto pt-8 mb-12">
       <h2 className="w-3/4 mx-auto text-center text-2xl sm:text-3xl font-semibold text-gray-700 px-5 mb-8 sm:mb-8 leading-snug">
         Making Charity Shop Exchange Possible
       </h2>
 
-      <div className="flex flex-wrap w-4/5 max-w-6xl items-stretch px-5 pt-5 pb-8 mx-auto mb-12 bg-gray-300">
-        
-        <div className="max-1/4">
-          <div className="flex items-start p-4 m-4">
-            <img className="h-12 w-12 mr-5 bg-gray-600 rounded-full" src={kelvin} alt="Kelvin" />
-          
-                <div>
-                        <h3 className="text-xl font-semibold text-gray-700">
-                          Kelvin Lockwood
-                        </h3>
-                        <div className="flex items-center">
-                          <a href="">
-                            <img className="p-1" src={twitter} alt="twitter" />
-                          </a>
-                          <a href="">
-                            <img className="p-1" src={github} alt="github" />
-                          </a>
-                          <a href="">
-                            <img className="p-1" src={discord} alt="discord" />
-                          </a>
-                        </div>
-                        <p className="text-xs text-gray-600">Placeholder for short bio.</p>
-                </div>
-        </div>
-        </div>
+      <div className="flex flex-wrap w-full max-w-6xl items-stretch px-5 pt-5 pb-8 mx-auto mb-12">
 
-
-        
-        <div className="max-1/4">
-        <div className="flex items-start p-4 m-4">
-                <img className="h-12 w-12 mr-4 bg-gray-600 rounded-full" src={blake} alt="Blake" />
-                <div>
-                        <h3 className="text-xl font-semibold text-gray-700">
-                          Blake Prouty
-                        </h3>
-                        <div className="flex items-center">
-                        <a href="">
-                          <img className="p-1" src={twitter} alt="twitter" />
-                        </a>
-                        <a href="">
-                          <img className="p-1" src={github} alt="github" />
-                        </a>
-                        <a href="">
-                          <img className="p-1" src={discord} alt="discord" />
-                        </a>
-                        </div>
-                        <p className="text-xs text-gray-600">Placeholder for short bio.</p>
-                </div>
-        </div>
-        </div>
-
-
-      <div className="max-1/4">
-        <div className="flex items-start p-4 m-4">
-          <img className="h-12 w-12 mr-4 bg-gray-600 rounded-full" src={dicky} alt="Dicky" />
-          <div>
-            <h3 className="text-xl font-semibold text-gray-700">Dicky Jiang</h3>
-            <div className="flex items-center">
-            <a href="">
-              <img className="p-1" src={twitter} alt="twitter" />
-            </a>
-            <a href="">
-              <img className="p-1" src={github} alt="github" />
-            </a>
-            <a href="">
-              <img className="p-1" src={discord} alt="discord" />
-            </a>
-            </div>
-            <p className="text-xs text-gray-600">Placeholder for short bio.</p>
-          </div>
-        </div>
-      </div>
-
-
-      <div className="max-1/4">
-        <div className="flex items-start p-4 m-4">
-          <img className="h-12 w-12 mr-4 bg-gray-600 rounded-full " src={matt} alt="Matt" />
-          <div >
-            <h3 className="text-xl font-semibold text-gray-700">Matt Doyle</h3>
-            <div className="flex items-center">
-
-            <a href="">
-              <img className="p-1" src={twitter} alt="twitter" />
-            </a>
-            <a href="">
-              <img className="p-1" src={github} alt="github" />
-            </a>
-            <a href="">
-              <img className="p-1" src={discord} alt="discord" />
-            </a>
-            </div>
-            <p className="text-xs text-gray-600">Placeholder for short bio.</p>
-          </div>
-        </div>
-      </div>
-
-
-      <div>
-        <div className="flex items-start p-4 m-4">
-          <img className="h-12 w-12 mr-4 bg-gray-600 rounded-full object-cover " src={marina} alt="marina" />
-          <div>
-            <h3 className="text-xl font-semibold text-gray-700">Marina Goto</h3>
-            <div className="flex items-center">
-            <a href="">
-              <img className="p-1" src={twitter} alt="twitter" />
-            </a>
-            <a href="">
-              <img className="p-1" src={github} alt="github" />
-            </a>
-            <a href="">
-              <img className="p-1" src={discord} alt="discord" />
-            </a>
-
-            </div>
-            <p className="text-xs text-gray-600">Placeholder for short bio.</p>
-          </div>
-        </div>
-      </div>
-
-      <div>
-        <div className="flex items-start p-4 m-4">
-          <img className="h-12 w-12 mr-4 bg-gray-600 rounded-full" src={generic} alt="generic" />
-          <div>
-            <h3 className="text-xl font-semibold text-gray-700">Rama Mani</h3>
-            <div className="flex items-center">
-
-            <a href="">
-              <img className="p-1" src={twitter} alt="twitter" />
-            </a>
-            <a href="">
-              <img className="p-1" src={github} alt="github" />
-            </a>
-            <a href="">
-              <img className="p-1" src={discord} alt="discord" />
-            </a>
-            </div>
-            <p className="text-xs text-gray-600"> Placeholder for short bio.</p>
-          </div>
-        </div>
-      </div>
-
-      </div>
-      <h2 className="w-3/4 mx-auto text-center text-2xl sm:text-3xl font-semibold  px-5 mb-8 sm:mb-12 leading-snug text-red-700">
-        Want to join the cause?
-      </h2>
-      <p className="w-4/5 mx-auto px-5 text-center text-sm sm:text-xl text-gray-700 font-semibold mb-12 sm:mb-20 leading-relaxed sm:leading-relax">
-        We are always looking for amazing people regardless of experience.
-        <div className="mt-8"> 
-        <button className="text-semibold text-red-700 font-semibold border-2 border-red-700 rounded-full px-5 py-2">Join the Force</button>
-        </div>
-      </p>
-      <h2 className="w-4/5 mx-auto px-5 text-center text-sm sm:text-xl text-gray-700 font-semibold mb-12 sm:mb-20 leading-relaxed sm:leading-relax">
-        Have an idea you'd like to contribute? A new service you'd like to see?
         <div>
-        <button className="text-semibold text-red-700 font-semibold border-2 border-red-700 rounded-full px-5 py-2 mt-8">Contact Us</button>
+          <div className="flex items-start p-4 m-4">
+            <img
+              className="h-12 w-12 mr-5 bg-gray-600 rounded-full"
+              src={kelvin}
+              alt="Kelvin"
+            />
+            <div>
+              <h3 className="text-xl font-semibold text-gray-700">
+                Kelvin Lockwood
+              </h3>
+              <div className="flex items-center">
+                <a href="">
+                  <img className="p-1" src={twitter} alt="twitter" />
+                </a>
+                <a href="">
+                  <img className="p-1" src={github} alt="github" />
+                </a>
+                <a href="">
+                  <img className="p-1" src={discord} alt="discord" />
+                </a>
+              </div>
+              <p className="text-xs text-gray-600">
+                Placeholder for short bio.
+              </p>
+            </div>
+          </div>
         </div>
-      </h2>
-      </container>
+
+        <div>
+          <div className="flex items-start p-4 m-4">
+            <img
+              className="h-12 w-12 mr-4 bg-gray-600 rounded-full"
+              src={blake}
+              alt="Blake"
+            />
+            <div>
+              <h3 className="text-xl font-semibold text-gray-700">
+                Blake Prouty
+              </h3>
+              <div className="flex items-center">
+                <a href="">
+                  <img className="p-1" src={twitter} alt="twitter" />
+                </a>
+                <a href="">
+                  <img className="p-1" src={github} alt="github" />
+                </a>
+                <a href="">
+                  <img className="p-1" src={discord} alt="discord" />
+                </a>
+              </div>
+              <p className="text-xs text-gray-600">
+                Placeholder for short bio.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="flex items-start p-4 m-4">
+            <img
+              className="h-12 w-12 mr-4 bg-gray-600 rounded-full"
+              src={dicky}
+              alt="Dicky"
+            />
+            <div>
+              <h3 className="text-xl font-semibold text-gray-700">
+                Dicky Jiang
+              </h3>
+              <div className="flex items-center">
+                <a href="">
+                  <img className="p-1" src={twitter} alt="twitter" />
+                </a>
+                <a href="">
+                  <img className="p-1" src={github} alt="github" />
+                </a>
+                <a href="">
+                  <img className="p-1" src={discord} alt="discord" />
+                </a>
+              </div>
+              <p className="text-xs text-gray-600">
+                Placeholder for short bio.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="flex items-start p-4 m-4">
+            <img
+              className="h-12 w-12 mr-4 bg-gray-600 rounded-full "
+              src={matt}
+              alt="Matt"
+            />
+            <div>
+              <h3 className="text-xl font-semibold text-gray-700">
+                Matt Doyle
+              </h3>
+              <div className="flex items-center">
+                <a href="">
+                  <img className="p-1" src={twitter} alt="twitter" />
+                </a>
+                <a href="">
+                  <img className="p-1" src={github} alt="github" />
+                </a>
+                <a href="">
+                  <img className="p-1" src={discord} alt="discord" />
+                </a>
+              </div>
+              <p className="text-xs text-gray-600">
+                Placeholder for short bio.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="flex items-start p-4 m-4">
+            <img
+              className="h-12 w-12 mr-4 bg-gray-600 rounded-full object-cover "
+              src={marina}
+              alt="marina"
+            />
+            <div>
+              <h3 className="text-xl font-semibold text-gray-700">
+                Marina Goto
+              </h3>
+              <div className="flex items-center">
+                <a href="">
+                  <img className="p-1" src={twitter} alt="twitter" />
+                </a>
+                <a href="">
+                  <img className="p-1" src={github} alt="github" />
+                </a>
+                <a href="">
+                  <img className="p-1" src={discord} alt="discord" />
+                </a>
+              </div>
+              <p className="text-xs text-gray-600">
+                Placeholder for short bio.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="flex items-start p-4 m-4">
+            <img
+              className="h-12 w-12 mr-4 bg-gray-600 rounded-full"
+              src={generic}
+              alt="generic"
+            />
+            <div>
+              <h3 className="text-xl font-semibold text-gray-700">Rama Mani</h3>
+              <div className="flex items-center">
+                <a href="">
+                  <img className="p-1" src={twitter} alt="twitter" />
+                </a>
+                <a href="">
+                  <img className="p-1" src={github} alt="github" />
+                </a>
+                <a href="">
+                  <img className="p-1" src={discord} alt="discord" />
+                </a>
+              </div>
+              <p className="text-xs text-gray-600">
+                {" "}
+                Placeholder for short bio.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
       
+        <div className="items-center sm:max-w-screen-xl mx-auto px-4 sm:px-12">
+
+          <div className="text-center text-2xl sm:text-3xl font-semibold px-5 mb-8 sm:mb-20 text-gray-700 tracking-wide">
+            Want to join the cause?
+            <p className="px-5 text-center text-sm sm:text-xl text-gray-600 font-medium mb-8 tracking-normal">
+              We are always looking for amazing people regardless of experience.
+            </p>
+            <button className="bg-red-700 py-2 sm:py-4 px-12 sm:px-20 text-sm sm:text-lg mt-4 sm:mt-0 font-medium py-1  rounded-full text-white border-2">
+              Join the Force
+            </button>
+          </div>
+        </div>
+      
+
+    
+      <div className="bg-red-700 pt-8 pb-20 ">
+
+        <div className="flex flex-col items-center w-full sm:max-w-screen-xl px-4 sm:px-12">
+
+          <div className="text-center text-xl sm:text-2xl font-medium px-5 mb-8 sm:mb-12 text-white tracking-wide leading-snug">
+            Have an idea you'd like to contribute? A new service you'd like to see?
+          </div>
+      
+      
+          <div className="w-64 py-2 sm:py-4 px-12 sm:px-20 text-sm sm:text-lg mt-4 font-medium py-1 rounded-full text-white border-2">
+              Contact Us
+          </div>
+
+
+        </div>
+      </div>
     </Layout>
   );
 }
