cecc576bb48438ddf223c67a2ca4f54101308e70
Matt Doyle <matt@elated.com>
2020-03-29 11:44:39 +1100

    Added Register page/route

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Register from "./pages/Register";
 import Faq from "./pages/Faq";
 import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
@@ -16,6 +17,9 @@ function App() {
       <Route path="/about">
         <About />
       </Route>
+      <Route path="/register">
+        <Register />
+      </Route>      
       <Route path="/faq">
         <Faq />
       </Route>      
