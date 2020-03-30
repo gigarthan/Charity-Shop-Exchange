f4f85b39a84972057204b45c49d895fb2fc2f8d4
Matt Doyle <matt@elated.com>
2020-03-30 15:13:55 +1100

    index.js now imports metropolis font

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import ScrollToTop from './components/ScrollToTop';
-
 import './assets/styles/tailwind.css';
-
+import 'typeface-metropolis';
 
 ReactDOM.render(
   <React.StrictMode>
