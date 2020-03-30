06ff83d76640c25dd42f652337f8ff835a44ee9b
Matt Doyle <matt@elated.com>
2020-03-30 12:27:04 +1100

    Pages now automatically scroll to top when visited

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
+import ScrollToTop from './components/ScrollToTop';
 
 import './assets/styles/tailwind.css';
 
@@ -9,6 +10,7 @@ import './assets/styles/tailwind.css';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
