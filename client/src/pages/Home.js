f5f67418825fd7507234f40b223b3334d2def52d
dicky <dickyjuwono@gmail.com>
2020-04-03 12:07:08 +0700

    add image new hero fixing how it works posittion add slightly on hover transition on how it works images

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -18,24 +18,34 @@ import boxwalk from "../assets/img/17431-package-delivery.gif";
 import starttext from "../assets/img/start text.png";
 import base from "../assets/img/base@1x.png";
 import door from "../assets/img/door@1x.png";
+import newhero from "../assets/img/newHeroImageonly.svg";
 
 export default function Home() {
   return (
     <Layout>
       {/* Hero section */}
       <div>
-        <div className="flex flex-col sm:flex-row max-w-full sm:max-w-screen-md pt-10 mb-8 sm:mb-0 sm:pt-32 mx-auto px-12 sm:px-10">
+        <div className="flex flex-col sm:flex-row sm:flex-stretch items-center sm:items-start max-w-full sm:max-w-screen-md md:max-w-screen-lg pt-10 mb-8 sm:mb-0 sm:pt-32 mx-auto px-12 sm:px-10">
           {/* hero left */}
-          <div className="flex flex-col items-start w-full sm:pr-4 sm:mb">
-            <h1 className="font-header text-3xl sm:text-5xl font-bold text-new-red leading-tight mb-2 sm:mb-4">
-              Fighting&nbsp;isolation with books and DVDs
+          <div className="flex flex-col items-start sm:w-1/2 md:w-2/3 sm:mr-2">
+            <h1 className="font-header text-3xl sm:text-4xl lg:text-5xl font-bold text-new-red leading-tight">
+              Fighting&nbsp;isolation<br></br> with books and DVDs
             </h1>
-            <h3 className="text-base sm:text-xl text-gray-600 font-medium mb-12 sm:mb-12">
-              Get a regular supply of things to read and watch. We collect a
+            <h3 className="text-xs mt-3 sm:text-sm md:text-md lg:text-lg text-gray-600 font-medium mb-12 sm:mb-12">
+              Get a regular supply of things to read and watch.
+              <span className="block">
+              We collect a
               selection of books and DVDs from local charity shops and deliver
               weekly, bi-weekly or monthly — it’s up to you.
+
+                </span>
             </h3>
           </div>
+          <div className="flex-1 ">
+            <img className="" src={newhero} alt="newhero" />
+          </div>
+        </div>
+      </div>
 
           {/* hero right */}
 
@@ -59,15 +69,9 @@ export default function Home() {
         </div> */}
 
           {/* ============== New Hero Door ===================== */}
-          <div className="flex flex-col items-center ">
-            <img className="" src={door} alt="door" />
 
-            <img className="" src={base} alt="hero" />
-          </div>
-        </div>
-      </div>
-      <div className="max-w-screen-md mx-auto pl-12 transition duration-500 ease-in-out transform hover:-translate-y-1">
-        <img className="" src={starttext} alt="text" />
+      <div className="max-w-screen-md mx-auto pl-20 sm:pl-2 transition duration-500 ease-in-out transform hover:-translate-y-1">
+        <img className="w-3/4 sm:w-auto" src={starttext} alt="text" />
       </div>
 
       {/* Input section */}
@@ -92,7 +96,8 @@ export default function Home() {
               <div className="flex-1 text-xl sm:text-2xl mt-3 font-semibold text-gray-700 leading-snug">
                 1. Start by telling us the things you like to read and watch.
               </div>
-              <div className="w-3/4 sm:w-1/2 flex-initial">
+              <div className="w-3/4 sm:w-2/5 flex-initial transition duration-500 ease-in-out transform hover:-translate-y-1
+">
                 <img className src={wish} alt="listening" />
               </div>
             </div>
@@ -102,18 +107,18 @@ export default function Home() {
                 2. We buy the items from your local charity shops and drop the
                 box at your door.
               </div>
-              <div className="w-48 sm:w-1/2">
+              <div className="w-48 sm:w-1/2 transition duration-500 ease-in-out transform hover:-translate-y-1">
                 <img className="ml-8 sm:mr-12" src={deliver} alt="deliver" />
               </div>
             </div>
 
-            <div className="flex flex-col-reverse items-center sm:flex-row py-4 px-3 mb-12">
+            <div className="flex flex-col-reverse items-start sm:flex-row py-4 px-3 mb-12">
               <div className="flex-1">
                 <div className="text-xl sm:text-2xl mt-3 font-semibold text-gray-700 leading-snug">
                   3. Enjoy moments of liberation without leaving home.
                 </div>
               </div>
-              <div className="w-3/4 sm:w-1/2">
+              <div className="w-3/4 sm:w-2/5 transition duration-500 ease-in-out transform hover:-translate-y-1">
                 <img className="sm:mr-32" src={enjoy} alt="enjoy" />
               </div>
             </div>
