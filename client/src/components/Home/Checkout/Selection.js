c4cdf409b36647d67c2e42967505cfa03d930488
Irshad Ansari <irshadjsr21@gmail.com>
2020-05-31 11:59:27 +0530

    Retain genre selection on reopenning checkout modal

--- client/src/components/Home/Checkout/Selection.js
+++ client/src/components/Home/Checkout/Selection.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import { Card, Tabs } from '@shopify/polaris';
-import Collapsable from '../Collapsable';
-import Button from '../../Button';
-import NumberFieldWithLabel from '../../NumberFieldWithLabel';
+import React, { Component } from "react";
+import { Card, Tabs } from "@shopify/polaris";
+import Collapsable from "../Collapsable";
+import Button from "../../Button";
+import NumberFieldWithLabel from "../../NumberFieldWithLabel";
 
-import shopping from '../../../assets/img/shopping.png';
+import shopping from "../../../assets/img/shopping.png";
 
-import '@vaadin/vaadin-radio-button';
-import RadioField from '../../RadioField';
+import "@vaadin/vaadin-radio-button";
+import RadioField from "../../RadioField";
 
 export default class Selection extends Component {
   constructor(props) {
@@ -18,55 +18,90 @@ export default class Selection extends Component {
       genresItems: props.genres,
       checkOutItems: {
         dvd: [],
-        books: [],
-      },
+        books: []
+      }
     };
   }
 
-  handleTabChange = (selectedTabIndex) =>
+  componentDidMount() {
+    const genreItems = this.props.genres;
+    const checkoutItems = this.props.formData.checkoutItems;
+
+    const keys = ['dvd', 'books'];
+
+    for(const key of keys) {
+      const checkoutKeyItems = checkoutItems[key];
+      const genreKeyItems = genreItems[key];
+
+      for(const item of checkoutKeyItems) {
+        if(item.quantity > 0) {
+          const index = genreKeyItems.findIndex(elem => elem.id === item.id);
+          if(index !== -1) {
+            genreKeyItems[index].value = item.quantity;
+          }
+        }
+      }
+    }
+
+    this.setState({
+      checkOutItems: checkoutItems,
+      genresItems: genreItems
+    });
+  }
+
+  handleTabChange = selectedTabIndex =>
     this.setState({ selected: selectedTabIndex });
 
   handleOnChange = (selectedId, value, tabSelected) => {
     let tempCheckoutObj = { ...this.state.checkOutItems };
 
-    let index = tempCheckoutObj[tabSelected].findIndex((x) => {
+    let index = tempCheckoutObj[tabSelected].findIndex(x => {
       return x.id === selectedId;
     });
 
     if (index === -1) {
       let temp = {
         id: selectedId,
-        quantity: parseInt(value),
+        quantity: parseInt(value)
       };
 
       tempCheckoutObj[tabSelected].push(temp);
     } else {
-      tempCheckoutObj[tabSelected][index]['quantity'] = value
+      tempCheckoutObj[tabSelected][index]["quantity"] = value;
     }
 
     let tempGenresObj = { ...this.state.genresItems };
     const elementIndex = tempGenresObj[tabSelected].findIndex(
-      (element) => element.id == selectedId,
+      element => element.id == selectedId
     );
     let updateGenreArray = [...tempGenresObj[tabSelected]];
     updateGenreArray[elementIndex] = {
       ...updateGenreArray[elementIndex],
-      value: parseInt(value),
+      value: parseInt(value)
     };
 
-    this.setState((prevState) => {
-      return {
-        ...prevState,
-        genresItems: {
-          ...prevState.genresItems,
-          [tabSelected]: updateGenreArray,
-        },
-        checkOutItems: tempCheckoutObj,
-      };
-    }, () => {
-      this.props.handleChange({ name: 'checkoutItems.dvd', value: this.state.checkOutItems && this.state.checkOutItems.dvd })
-      this.props.handleChange({ name: 'checkoutItems.books', value: this.state.checkOutItems && this.state.checkOutItems.books })
-    });
+    this.setState(
+      prevState => {
+        return {
+          ...prevState,
+          genresItems: {
+            ...prevState.genresItems,
+            [tabSelected]: updateGenreArray
+          },
+          checkOutItems: tempCheckoutObj
+        };
+      },
+      () => {
+        this.props.handleChange({
+          name: "checkoutItems.dvd",
+          value: this.state.checkOutItems && this.state.checkOutItems.dvd
+        });
+        this.props.handleChange({
+          name: "checkoutItems.books",
+          value: this.state.checkOutItems && this.state.checkOutItems.books
+        });
+      }
+    );
   };
 
   render() {
@@ -74,15 +109,15 @@ export default class Selection extends Component {
 
     const tabs = [
       {
-        id: 'books',
-        content: 'Books',
-        panelID: 'Books',
+        id: "books",
+        content: "Books",
+        panelID: "Books"
       },
       {
-        id: 'dvd',
-        content: 'DVDs',
-        panelID: 'DVD',
-      },
+        id: "dvd",
+        content: "DVDs",
+        panelID: "DVD"
+      }
     ];
 
     return (
@@ -102,10 +137,7 @@ export default class Selection extends Component {
               <div className="flex flex-row flex-wrap items-center justify-space-between">
                 {genresItems[tabs[selected].id].map((genre, index) => {
                   return (
-                    <div
-                      className="numberfild-width"
-                      key={genre.id}
-                    >
+                    <div className="numberfild-width" key={genre.id}>
                       <div className="p-2 w-full">
                         <NumberFieldWithLabel
                           selectedTab={tabs[selected].id}
