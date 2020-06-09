c02505a5418d88952ce6b182841d6e925b45578b
engjellerza@hotmail.com <engjellerza@hotmail.com>
2020-06-09 20:15:03 +0300

    login-button update

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -5,8 +5,8 @@ export default function LoginButton() {
     return (
     <a href="javascript:void(0)" data-billsby-type="account">
         <div
-            className="z-50 login-button right flex items-center top-0 right-0 mt-2 
-        sm:mt-4 mr-4 rounded-md bg-white absolute py-1 px-5">
+            className="z-50 login-button r flex items-center top-0 right-0 mt-2 
+        sm:mt-4 rounded-md bg-white absolute py-1 px-5">
             <div className=" text-sm font-medium red text-center text-gray-700">
                 Log In
         </div>
