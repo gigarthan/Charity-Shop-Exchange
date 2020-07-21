e142e61f5f57a9ab03adecfb19b06569e1988c3f
Haseeb Majid <hello@haseebmajid.dev>
2020-07-21 12:12:27 +0100

    fix: Prettier Code Formatter
    Fixed code formatting for `Footer.js`.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -55,23 +55,31 @@ export default function Footer() {
         </div>
         <div className="flex flex-col sm:flex-row text-xs w-full lg:w-auto flex-wrap justify-center text-center mt-4 sm:mt-0">
           <div className="flex flex-col sm:flex-row">
-            <Link className="mr-0 sm:mr-4 text-gray-400 hover:text-white " to="/privacy">
+            <Link
+              className="mr-0 sm:mr-4 text-gray-400 hover:text-white "
+              to="/privacy">
               Privacy
             </Link>
             <span className="hidden sm:inline">|</span>
-            <Link className="mr-0 sm:mx-4 text-gray-400 hover:text-white" to="/terms">
+            <Link
+              className="mr-0 sm:mx-4 text-gray-400 hover:text-white"
+              to="/terms">
               Terms
             </Link>
             <span className="hidden sm:inline">|</span>
           </div>
           <div className="flex flex-col sm:flex-row">
-            <Link className="mr-0 sm:mx-4 text-gray-400 hover:text-white" to="/customer-terms">
+            <Link
+              className="mr-0 sm:mx-4 text-gray-400 hover:text-white"
+              to="/customer-terms">
               Customer Terms and Conditions
             </Link>
             <span className="hidden sm:inline">|</span>
           </div>
           <div className="block">
-            <a className="mr-0 sm:mx-4 text-gray-400 hover:text-white" href="https://icons8.com">
+            <a
+              className="mr-0 sm:mx-4 text-gray-400 hover:text-white"
+              href="https://icons8.com">
               How it works svgs by icon8
             </a>
           </div>
