b575a01b607966d3475071427670cad86c267049
Yohanathan-Kugavathanan <kugajpx2012@gmail.com>
2020-09-28 20:12:54 +0530

    template page extra header added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import TemplateHeader from '../templateHeader';
+
 import Footer from './Footer';
 import Head from './Head';
 import Header from './Header';
 
-const Layout = ({ children }) => (
+const Layout = ({ children, isHide }) => (
   <div className="App overflow-x-hidden">
     <Head />
     <Header />
+    {isHide ? <TemplateHeader /> : null}
     {children}
-    <Footer />
+    <Footer isHide={isHide} />
   </div>
 );
 
