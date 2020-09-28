da65fdf66165c75b2d1f000577cdab2ec22b80ec
gigarthan <gigajpx@gmail.com>
2020-09-28 23:13:48 +0530

    fix: darker hover shade on template

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -63,13 +63,17 @@ export default function Footer({ isHide }) {
         <div className="flex flex-col sm:flex-row text-xs w-full lg:w-auto flex-wrap justify-center text-center mt-4 sm:mt-0">
           <div className="flex flex-col sm:flex-row">
             <Link
-              className="mr-0 sm:mr-4 text-gray-400 hover:text-white "
+              className={`mr-0 sm:mr-4 text-gray-400 ${
+                isHide ? 'hover:text-gray-700' : 'hover:text-white'
+              } `}
               to="/privacy">
               Privacy
             </Link>
             <span className="hidden sm:inline">|</span>
             <Link
-              className="mr-0 sm:mx-4 text-gray-400 hover:text-white"
+              className={`mr-0 sm:mx-4 text-gray-400 ${
+                isHide ? 'hover:text-gray-700' : 'hover:text-white'
+              }`}
               to="/terms">
               Terms
             </Link>
@@ -77,19 +81,21 @@ export default function Footer({ isHide }) {
           </div>
           <div className="flex flex-col sm:flex-row">
             <Link
-              className="mr-0 sm:mx-4 text-gray-400 hover:text-white"
+              className={`mr-0 sm:mx-4 text-gray-400 ${
+                isHide ? 'hover:text-gray-700' : 'hover:text-white'
+              }`}
               to="/customer-terms">
               Customer Terms and Conditions
             </Link>
-            <span className="hidden sm:inline">|</span>
+            {/* <span className="hidden sm:inline">|</span> */}
           </div>
-          <div className="block">
+          {/* <div className="block">
             <a
               className="mr-0 sm:mx-4 text-gray-400 hover:text-white"
               href="https://icons8.com">
               How it works svgs by icon8
             </a>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
